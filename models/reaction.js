4ae6618319ca7bab32532832c5ffa80880cddeae
Mark T <aydenthos@gmail.com>
2023-09-22 10:45:14 -0700

    all models built

---
+++ models/reaction.js
@@ -0,0 +1,33 @@
+const { get } = require('cypress/types/lodash');
+const { Schema } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: 'Reaction is required',
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: 'Username is required'
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+
+// export the ReactionSchema
+module.exports = ReactionSchema;
