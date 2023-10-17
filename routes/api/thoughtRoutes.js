6a1e0c89c2b75f13a7c36584caaaef3b9f8a67bc
Mark T <aydenthos@gmail.com>
2023-10-17 08:45:54 -0700

    package changes and some thoughtRoute changes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -19,6 +19,9 @@ router.route('/:userId').post(addThought);
 router.route('/:id').get(getThoughtById).put(updateThought).delete(removeThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
 
 module.exports = router;
